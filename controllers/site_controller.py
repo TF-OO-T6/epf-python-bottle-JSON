35c9e229f0a2d83edbdaf19b1b2592229bc8005f
pedrohenriquerodri <tiagojam345@gmail.com>
2025-11-25 05:27:53 -0300

    arrumei as fotos e algumas alteraçoes no header

--- controllers/site_controller.py
+++ controllers/site_controller.py
@@ -1,8 +1,8 @@
-# ARQUIVO: controllers/site_controller.py
 from bottle import template
+from controllers.base_controller import BaseController
 from services.local_service import LocalService
 
-class SiteController:
+class SiteController(BaseController):
     def __init__(self, app):
         self.bottle = app
         self.service = LocalService()
@@ -11,7 +11,26 @@ class SiteController:
         dados_locais = self.service.get_all()
         return template('views/home', locais=dados_locais)
 
+    # --- MUDANÇA AQUI: Retornar apenas texto para teste ---
+    def pagina_mapas(self):
+        return "FUNCIONOU: PÁGINA DE MAPAS"
+
+    def pagina_gastronomia(self):
+        return "FUNCIONOU: PÁGINA DE GASTRONOMIA"
+
+    def pagina_eventos(self):
+        return "FUNCIONOU: PÁGINA DE EVENTOS"
+
+    def pagina_museus(self):
+        return "FUNCIONOU: PÁGINA DE MUSEUS"
+
     def setup_routes(self):
+        print("🔗 Configurando rotas...")
+        self.bottle.route('/portal', method='GET', callback=self.index)
         self.bottle.route('/', method='GET', callback=self.index)
         
-        self.bottle.route('/users', method='GET', callback=self.index)
+        # Rotas dos botões
+        self.bottle.route('/mapas', method='GET', callback=self.pagina_mapas)
+        self.bottle.route('/gastronomia', method='GET', callback=self.pagina_gastronomia)
+        self.bottle.route('/eventos', method='GET', callback=self.pagina_eventos)
+        self.bottle.route('/museus', method='GET', callback=self.pagina_museus)
