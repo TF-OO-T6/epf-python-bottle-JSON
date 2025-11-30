2128de7cc393645fb71378498e25a605a008d77b
smatheusnf <mnunes.ferreirasilva@gmail.com>
2025-11-30 17:13:38 -0300

    Atualiza a criacao de ID

--- services/user_service.py
+++ services/user_service.py
@@ -7,18 +7,18 @@ class UserService:
         self.db_path = 'data/users.json'
         
         if not os.path.exists(self.db_path):
-            with open(self.db_path, 'w') as f:
+            with open(self.db_path, 'w', encoding='utf-8') as f:
                 json.dump([], f)
 
     def _load_users(self):
         try:
-            with open(self.db_path, 'r') as f:
+            with open(self.db_path, 'r', encoding='utf-8') as f:
                 return json.load(f)
         except:
             return []
 
     def _save_users(self, users):
-        with open(self.db_path, 'w') as f:
+        with open(self.db_path, 'w', encoding='utf-8') as f:
             json.dump(users, f, indent=4)
 
     def create_user(self, name, email, password):
@@ -28,7 +28,9 @@ class UserService:
             if u['email'] == email:
                 return False
         
-        new_user = User(name, email, password)
+        new_id = len(users) + 1
+        new_user = User(name, email, password, id=new_id)
+        
         users.append(new_user.to_dict())
         self._save_users(users)
         return True
