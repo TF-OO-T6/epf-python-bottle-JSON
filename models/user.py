ff090df66860f62c5c48446dae72307f12121d61
smatheusnf <mnunes.ferreirasilva@gmail.com>
2025-11-30 17:10:07 -0300

    Adiciona id ao usuario

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 class User:
-    def __init__(self, name, email, password):
+    def __init__(self, name, email, password, id=None):
+        self.id = id
         self.name = name
         self.email = email
         self.password = password
 
     def to_dict(self):
         return {
+            "id": self.id,
             "name": self.name,
             "email": self.email,
             "password": self.password
